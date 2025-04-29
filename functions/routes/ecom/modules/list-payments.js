89abed2855bb94b5b361bd44fa7b2aa27417f762
Leonardo Matos <leomp120894@gmail.com>
2025-04-29 03:27:29 -0300

    fix: preset sop script src and clearsale fingerprint app id on js client container html

--- functions/routes/ecom/modules/list-payments.js
+++ functions/routes/ecom/modules/list-payments.js
@@ -181,10 +181,10 @@ exports.post = async ({ appSdk }, req, res) => {
 
         gateway.js_client = {
           script_uri: 'https://ecom-braspag.web.app/dist/card-client.min.js',
+          container_html: `<script>window._braspagSopSrc="${baseScriptUri}/post/scripts/silentorderpost-1.0.min.js"</script>`,
           onload_expression: `window._braspagAccessToken="${accessTokenSOP}";` +
             `window._braspagIsSandbox=${scriptIsSandBox};` +
-            `window._braspagFingerprintApp="${fingerprintApp}";` +
-            `window._braspagSopSrc="${baseScriptUri}/post/scripts/silentorderpost-1.0.min.js"`,
+            `window._braspagFingerprintApp="${fingerprintApp}";`,
           cc_hash: {
             function: '_braspagHashCard',
             is_promise: true
