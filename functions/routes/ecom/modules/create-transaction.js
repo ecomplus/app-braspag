a3dc39ad340e41e129efa41e5275fd0f247b5264
Leonardo Matos <leomp120894@gmail.com>
2025-12-04 21:51:16 -0300

    chore(deps): lock file maintenance

--- functions/routes/ecom/modules/create-transaction.js
+++ functions/routes/ecom/modules/create-transaction.js
@@ -4,6 +4,7 @@ const axios = require('../../../lib/braspag/create-axios')
 const { parseStatus } = require('../../../lib/braspag/parse-utils')
 const bodyBraspag = require('../../../lib/braspag/payload-to-transaction')
 const addInstallments = require('../../../lib/payments/add-payments')
+
 exports.post = async ({ appSdk, admin }, req, res) => {
   /**
    * Requests coming from Modules API have two object properties on body: `params` and `application`.
